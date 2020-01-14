f5e2b53718d13d546a33bb85b489c993e96c78d5
Ruben Lukes <lukesruben@gmail.com>
2020-01-14 22:41:57 +0100

    added service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthService } from "./auth/auth.service";
+import { LoggingService } from "./logging.service";
 
 @Component({
   selector: "app-root",
@@ -7,9 +8,10 @@ import { AuthService } from "./auth/auth.service";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements OnInit {
-  constructor(private as: AuthService) {}
+  constructor(private as: AuthService, private ls: LoggingService) {}
 
   ngOnInit(): void {
     this.as.autoLogin();
+    this.ls.printLog("hello from appComponent");
   }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,27 +1,30 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Ingredient } from "./../shared/ingredient.model";
 import { ShoppingListSerivce } from "./shoppinglist.serivce";
-import { Subscription } from 'rxjs';
+import { Subscription } from "rxjs";
+import { LoggingService } from "./../logging.service";
 
 @Component({
   selector: "app-shopping-list",
   templateUrl: "./shopping-list.component.html",
   styleUrls: ["./shopping-list.component.scss"]
 })
-export class ShoppingListComponent implements OnInit,OnDestroy {
-
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
-  private subscription:Subscription;
+  private subscription: Subscription;
 
-  constructor(private ss: ShoppingListSerivce) {}
+  constructor(private ss: ShoppingListSerivce, private ls: LoggingService) {}
 
   ngOnInit() {
     this.ingredients = this.ss.getIngredients();
-    this.subscription =this.ss.ingredientChanged.subscribe((data: Ingredient[]) => {
-      this.ingredients = data;
-    });
+    this.subscription = this.ss.ingredientChanged.subscribe(
+      (data: Ingredient[]) => {
+        this.ingredients = data;
+      }
+    );
+    this.ls.printLog("hello from shoppinglist component");
   }
-  onEditItem(i:number){
+  onEditItem(i: number) {
     this.ss.startedEditing.next(i);
   }
 
